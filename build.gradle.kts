8cd86f27a863b5cab7b59c8c62b0f4382dd8a7dd
seonwoo_jung <laborlawseon@gmail.com>
2023-05-24 14:17:08 +0900

    - TDD 학습중

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,8 +1,10 @@
 plugins {
     id("java")
+    id("org.springframework.boot") version "2.7.11"
+    id("io.spring.dependency-management") version "1.0.15.RELEASE"
 }
 
-group = "hellojpa"
+group = "tdd-start"
 version = "1.0-SNAPSHOT"
 
 repositories {
@@ -12,6 +14,8 @@ repositories {
 dependencies {
     testImplementation(platform("org.junit:junit-bom:5.9.1"))
     testImplementation("org.junit.jupiter:junit-jupiter")
+    testCompileOnly("org.projectlombok:lombok")
+    testAnnotationProcessor("org.projectlombok:lombok")
 }
 
 tasks.test {
